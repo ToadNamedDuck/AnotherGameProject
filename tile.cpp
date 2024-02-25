93806c8bc5e8664d7f7a1c1d304b4f2b0c3c744b
Jason Harris <lord.jtharris@gmail.com>
2024-02-24 18:34:23 -0600

    reformat and remove return of number values - checking every bit of information in the file

--- tile.cpp
+++ tile.cpp
@@ -17,7 +17,8 @@
 struct BMPHeader {
 	uint16_t signature; // BM
 	uint32_t fileSize;
-	uint32_t reserved;
+	uint16_t reserved1;
+	uint16_t reserved2;
 	uint32_t dataOffset;
 };
 struct DIBHeader {
@@ -32,6 +33,12 @@ struct DIBHeader {
 	int32_t verticalResolution;
 	uint32_t numColors;
 	uint32_t importantColors;
+};
+struct BitMasks {
+	uint32_t redMask;
+	uint32_t greenMask;
+	uint32_t blueMask;
+	uint32_t alphaMask;
 };
 #pragma pack(pop)
 
@@ -66,13 +73,49 @@ public:
 		//Read the bmpHeader
 		BMPHeader bmpHeader;
 		file.read(reinterpret_cast<char*>(&bmpHeader), sizeof(BMPHeader));
+		std::cout << "--------BMP HEADER START--------" << std::endl;
+		std::cout << "Signature: " << bmpHeader.signature << std::endl;//Should be "BM"
+		std::cout << "File Size: " << bmpHeader.fileSize << std::endl;//Bytes
+		std::cout << "Reserved 1: " << bmpHeader.reserved1 << std::endl;//
+		std::cout << "Reserved 2: " << bmpHeader.reserved2 << std::endl;//
+		std::cout << "Data Offset Location: " << bmpHeader.dataOffset << std::endl;//
+		std::cout << "--------BMP HEADER END--------" << std::endl;
+
 		//Read dibHeader
 		DIBHeader dibHeader;
 		file.read(reinterpret_cast<char*>(&dibHeader), sizeof(DIBHeader));
-		//loop through the region selected and get the pixel data.
+		std::cout << "--------DIB HEADER START--------" << std::endl;
+		std::cout << "Header Size: " << dibHeader.headerSize << std::endl;//V5
+		std::cout << "Width: " << dibHeader.width << std::endl;//
+		std::cout << "Height: " << dibHeader.height << std::endl;//
+		std::cout << "Color Planes: " << dibHeader.colorPlanes << std::endl;//Should be 1
+		std::cout << "Bit Depth: " << dibHeader.bitsPerPixel << std::endl;//Should be 32
+		std::cout << "Compression: " << dibHeader.compressionMethod << std::endl;//Turns out the file is actually compression value 3 - BI_BITFIELDS
+		std::cout << "Image Size: " << dibHeader.imageSize << std::endl;//
+		std::cout << "Horizontal Resolution: " << dibHeader.horizontalResolution << std::endl;//
+		std::cout << "Vertical Resolution: " << dibHeader.verticalResolution << std::endl;//
+		std::cout << "Number of Colors: " << dibHeader.numColors << std::endl;//
+		std::cout << "Important Colors: " << dibHeader.importantColors << std::endl;//Probably 0
+		std::cout << "--------DIB HEADER END--------" << std::endl;
 
+		//Retrieve bitmask info
+		BitMasks masks;
+		file.read(reinterpret_cast<char*>(&masks), sizeof(BitMasks));
+		std::cout << "--------BITMASK INFORMATION START--------" << std::endl;
+		std::cout << "Red Mask: " << std::hex << masks.redMask << std::endl;
+		std::cout << "Green Mask: " << masks.greenMask << std::endl;
+		std::cout << "Blue Mask: " << masks.blueMask << std::endl;
+		std::cout << "Alpha Mask: " << masks.alphaMask << std::endl;
+		std::cout << std::dec << "--------BITMASK INFORMATION END--------" << std::endl;
+
+		if (dibHeader.height > 0) {
+			dibHeader.height = -dibHeader.height;
+		}
+
+		//loop through the region selected and get the pixel data.
 		int rowSize = floor((dibHeader.bitsPerPixel * dibHeader.width + 31) / 32) * 4;  // Calculate row size
-		file.seekg(bmpHeader.dataOffset, std::ios::beg);
+		std::cout << "Row Size: " << rowSize << std::endl;
+		//file.seekg(bmpHeader.dataOffset, std::ios::beg);
 
 		for (int y = sprite_start_y; y < sprite_start_y + 16; y++) {
 			for (int x = sprite_start_x; x < sprite_start_x + 16; x++) {
@@ -80,23 +123,8 @@ public:
 				int bytePosition = y * rowSize + x * (dibHeader.bitsPerPixel / 8);
 				std::cout << "Byte Position: " << bytePosition << std::endl;
 				// Move the file reader to the correct position
-				file.seekg(bytePosition, std::ios::cur);
-				// Read RGBA values
-				char pixel[4];
-				file.read(pixel, sizeof(pixel));
-
-				// Access RGBA values
-				unsigned char blue = static_cast<unsigned char>(pixel[0]);
-				unsigned char green = static_cast<unsigned char>(pixel[1]);
-				unsigned char red = static_cast<unsigned char>(pixel[2]);
-				unsigned char alpha = static_cast<unsigned char>(pixel[3]);
-
-
-				std::cout << "Pixel at (" << x << ", " << y << "): ";
-				std::cout << "R=" << static_cast<int>(red) << ", ";
-				std::cout << "G=" << static_cast<int>(green) << ", ";
-				std::cout << "B=" << static_cast<int>(blue) << ", ";
-				std::cout << "A=" << static_cast<int>(alpha) << std::endl;
+				//file.seekg(bytePosition + bmpHeader.dataOffset, std::ios::beg);
+				std::cout << "Pixel at X: " << x << " | Y: " << y << std::endl;
 			}
 		}
 
